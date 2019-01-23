833ab7315b19fe2d01fbdd0e8c62159e741cda07
github username <github email address>
2019-01-22 21:39:54 -0500

    Initial header component created and added to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <Header />
       </div>
     );
   }
