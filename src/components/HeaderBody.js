7790a0352034a92011a4836ef0f240738b791487
github username <github email address>
2019-02-08 01:22:27 -0500

    HeaderBody a functional component now

--- src/components/HeaderBody.js
+++ src/components/HeaderBody.js
@@ -1,14 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class HeaderBody extends Component {
-  render() {
-    return (
+const HeaderBody = ({ data }) => (
       <div className="headerBody">
-        <h3><b>{this.props.data.default.communities.length}</b></h3>
-        <h5><b>Communities</b> in <b>{this.props.data.default.name}</b></h5>
+        <h3><b>{data.default.communities.length}</b></h3>
+        <h5><b>Communities</b> in <b>{data.default.name}</b></h5>
       </div>
     )
-  }
-}
-
+ 
 export default HeaderBody;
