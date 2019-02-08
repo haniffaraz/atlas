fcee4b7293811eb68dd7f260f612ba7671b75a29
github username <github email address>
2019-02-08 01:21:49 -0500

    Make header into a functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Table from './Table';
 import HeaderBody from './HeaderBody';
 
-class Header extends Component {
-  render() {
-    return (
+const Header = ({ data }) => (
       <div className="header">
         <div className="title">
           <h6 className="list">LIST OF COMMUNITIES</h6>
-          <HeaderBody data={this.props.data}/>
-          <Table communities={this.props.data.default.communities}/>
+          <HeaderBody data={data}/>
+          <Table communities={data.default.communities}/>
         </div>
       </div>
     )
-  }
-}
 
 export default Header;
